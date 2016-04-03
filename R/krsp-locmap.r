102d4fdeb7e46386394a06d47098d83698c52710
Matt Strimas-Mackey <mstrimas@gmail.com>
2016-04-03 09:31:36 -0700

    added from and to dates to locmap

--- R/krsp-locmap.r
+++ R/krsp-locmap.r
@@ -6,9 +6,18 @@
 #' mouseover. Plotting is done using \code{ggvis}, which creates an HTML object
 #' that pops up in the Viewer pane in RStudio.
 #'
+#' Records can be further filtered to a date range using \code{from_date} and
+#' \code{to_date} arguments. If both dates are provided, they must be in the
+#' same year, and \code{year} is ignored. If one of the two is provided it must
+#' be in the same year as the \code{year} argument.
+#'
 #' @param con Connection to KRSP database
 #' @param grid character; a single grid to map
-#' @param year integer; defaults to current year
+#' @param year integer
+#' @param from_date Date object or character representation of date in YMD format
+#'   (e.g. "2016-05-25"); only show records that are on or after this date
+#' @param to_date Date object or character representation of date in YMD format
+#'   (e.g. "2016-05-25"); only show records that are on or before this date
 #' @param data logical; if TRUE return data frame instead of plotting
 #'
 #' @return Displays and returns a \code{ggvis} plot of rattle locations, unless
@@ -20,18 +29,44 @@
 #' krsp_locmap(con, "JO", 2014, data = TRUE) %>%
 #'   head
 #' krsp_locmap(con, "KL", 2015)
-krsp_locmap <- function(con, grid, year) {
+#' krsp_locmap(con, "SU", 2014, from_date = "2014-04-01")
+#' krsp_locmap(con, "AG", from_date = "2014-04-01", to_date = "2014-04-30")
+krsp_locmap <- function(con, grid, year, from_date, to_date, data) {
   UseMethod("krsp_locmap")
 }
 
 #' @export
-krsp_locmap.krsp <- function(con, grid, year = current_year(), data = FALSE) {
-  # assertions
-  assertthat::assert_that(assertthat::is.count(year),
-                          all(year >= 1984),
-                          all(year <= current_year()),
+krsp_locmap.krsp <- function(con, grid, year, from_date, to_date,
+                             data = FALSE) {
+  assertthat::assert_that(missing(year) || assertthat::is.count(year),
+                          missing(from_date) || length(from_date) == 1,
+                          missing(to_date) || length(to_date) == 1,
                           length(grid) == 1,
                           grid %in% valid_grids())
+  # convert dates
+  if (!missing(from_date)) {
+    from_date <- suppressWarnings(as.Date(lubridate::ymd(from_date)))
+    assertthat::assert_that(!is.na(from_date))
+  }
+  if (!missing(to_date)) {
+    to_date <- suppressWarnings(as.Date(lubridate::ymd(to_date)))
+    assertthat::assert_that(!is.na(to_date))
+  }
+
+  # handle various permutations of provided dates and year
+  if (missing(from_date) && missing(to_date)) {
+    assertthat::assert_that(assertthat::is.count(year))
+  } else if (!missing(from_date) && !missing(to_date)) {
+    year <- lubridate::year(from_date)
+    assertthat::assert_that(lubridate::year(to_date) == year)
+  } else if (!missing(from_date) && missing(to_date)) {
+    assertthat::assert_that(lubridate::year(from_date) == year)
+  } else if (missing(from_date) && !missing(to_date)) {
+    assertthat::assert_that(lubridate::year(to_date) == year)
+  }
+  assertthat::assert_that(assertthat::is.count(year),
+                          all(year >= 1984),
+                          all(year <= current_year()))
 
   year <- as.integer(year)
   grid_choice <- grid
@@ -57,11 +92,24 @@ krsp_locmap.krsp <- function(con, grid, year = current_year(), data = FALSE) {
                     squirrel_id = as.integer(squirrel_id),
                     x = loc_to_numeric(locx),
                     y = suppressWarnings(round(as.numeric(locy), 1)),
+                    date = suppressWarnings(as.Date(lubridate::ymd(date))),
+                    colorlft = ifelse(is.na(colorlft), "-", colorlft),
+                    colorrt = ifelse(is.na(colorrt), "-", colorrt),
+                    taglft = ifelse(is.na(taglft), "-", taglft),
+                    colorrt = ifelse(is.na(colorrt), "-", colorrt),
                     colours = paste(colorlft, colorrt, sep = "/"),
                     tags = paste(taglft, tagrt, sep = "/")) %>%
     filter(!is.na(x), !is.na(y)) %>%
     mutate(id = row_number()) %>%
     select(id, squirrel_id, x, y, grid, sex, colours, tags, date, trap_date)
+
+  # date filtering
+  if (!missing(from_date)) {
+    results <- filter(results, date >= from_date)
+  }
+  if (!missing(to_date)) {
+    results <- filter(results, date <= to_date)
+  }
 
   # skip plotting and return data frame instead
   if (data) {
